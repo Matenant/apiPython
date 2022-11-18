80205f107421508fce49f0c3a503d9dddea2bd33
unknown <matheo.faivre@sfr.fr>
2022-11-18 23:04:50 +0100

    Fichier webservice

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+from flask import Flask, jsonify, request
+from pathlib import Path
+import base64
 
 # create the application object
 app = Flask(__name__)
@@ -9,9 +11,9 @@ app = Flask(__name__)
 def home():
     return "API pour les cours de l'IUT"  # return a string
 
-@app.get('/infoSvg', methods=['GET'])
+@app.get('/infoSvg')
 def infoSvg():
-    infoSvg = flask.jsonify([
+    infoSvg = jsonify([
         { "id": 1, "chemin": "svg/inspection.svg", "nom": "Inspection", "largeur": 66.86, "hauteur": 69.7, "echelle": 1.7, "origineX": 14.2, "origineY": 69.7 },
         { "id": 2, "chemin": "svg/cn.svg", "nom": "Cn", "largeur": 63.14, "hauteur": 82.689, "echelle": 1.2, "origineX": 17.79, "origineY": 82.689 },
         { "id": 3, "chemin": "svg/perceuse.svg", "nom": "Perceuse", "largeur": 40.58, "hauteur": 79.18, "echelle": 1.6, "origineX": 13.99, "origineY": 79.18 },
@@ -20,8 +22,34 @@ def infoSvg():
         { "id": 6, "chemin": "svg/robot.svg", "nom": "Robot", "largeur": 78.78, "hauteur": 85.51, "echelle": 1.6, "origineX": 29.76, "origineY": 85.51 },
         { "id": 7, "chemin": "svg/fraiseuse.svg", "nom": "Fraiseuse", "largeur": 80.13, "hauteur": 98.88, "echelle": 1.33, "origineX": 15.44, "origineY": 98.88 }
     ])
-    infoSvg.headers.add('Access-Control-Allow-ORigin', '*')
+    infoSvg.headers.add('Access-Control-Allow-Origin', '*')
     return infoSvg, 200  # render a template
+
+@app.get('/file')
+def getCN():
+    json = request.get_json()
+    path = 'files/' + json["fileName"]
+
+    obj = Path(path)
+
+    if obj.exists():
+        file = open(path, 'rb')
+
+        encoded_string = base64.b64encode(file.read())
+        svg = jsonify({
+            "success": True,
+            "fileName": json["fileName"],
+            "file": encoded_string.decode("utf-8")
+        })
+        code = 200 
+    else:
+        svg = jsonify({
+            "success": False,
+            "message": "Erreur lors de la récupération du fichier"
+        })
+        code = 404
+    svg.headers.add('Access-Control-Allow-Origin', '*')
+    return svg, code
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
